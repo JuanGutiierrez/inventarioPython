0d4915c8f1d46ba11d65e0c075e12caf2f233d08
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-08-29 08:06:41 -0500

    feat: se crea ejemplo de ista de diccionarios

---
+++ diccionario.py
@@ -0,0 +1,34 @@
+productoTerminado1 = {
+    'referencia':5087,
+    'marca':"Americanino",
+    'descripcion':"chompa de acampar",
+    'color':"naranja",
+    'costoUnitario':100000,
+    'disponible':True,
+    'costoVenta':850000,
+    'puntosVenta':['cc mayorcar','terminal norte','puerta del norte','centro de la moda']
+}
+
+productoTerminado2 = {
+    'referencia':5088,
+    'marca':"Americanino",
+    'descripcion':"Camiseta Polo",
+    'color':"Azul oscuro",
+    'costoUnitario':30000,
+    'disponible':True,
+    'costoVenta':150000,
+    'puntosVenta':['cc mayorcar','terminal norte','puerta del norte','centro de la moda']
+}
+
+#Creando una lisda de diccionarios
+productos=[productoTerminado1,productoTerminado2]
+
+#Recorriendo una lista con ciclo FOR
+'''for contador in range(10):
+    print(contador)'''
+
+for producto in productos:
+    for puntoVenta in producto["puntosVenta"]:
+        print(puntoVenta)
+    #print(producto["puntosVenta"])
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
---
+++ tiendasLaGanga.py
@@ -0,0 +1,49 @@
+'''Menu de opciones
+1. Ingresar producto bodega
+2. Verificar los productos en bodega
+3. Buscar un producto en la bodega
+4. Editar un producto en la bodega
+5. Retirar un producto de la bodega
+6. Salir de la bodega'''
+
+#Producto: nombre, codigo, decripcion, foto, precio, cantidadEnBodega,fechaEntradaBodega
+
+opcion=0
+print ("Tiendas el Gangazo\n********************")
+print("1. Ingresar producto bodega")
+print("2. Verificar los productos en bodega")
+print("3. Buscar un producto en la bodega")
+print("4. Editar un producto en la bodega")
+print("5. Retirar un producto de la bodega")
+print("6. Salir de la bodega\n********************")
+
+productos = []
+
+while(opcion != 6):
+    producto={}
+    opcion = int(input("Digita la opcion deseada: "))
+    if opcion == 1:
+        producto["nombre"] = input("Digita el nombre del producto: ")
+        producto["codigo"] = int(input("Digita el codigo del producto: "))
+        producto["descripcion"] = input("Digita la decripcion del producto: ")
+        producto["foto"] = input("Adjunte el URL de la foto del producto: ")
+        producto["precio"] = float(input("Digite el precio del producto: "))
+        producto["cantidadEnBodega"] = int(input("Digite el stock del producto: "))
+        producto["fechaEntradaBodega"] = (input("Digite la fecha del producto: "))
+        
+        productos = [producto]
+    elif opcion ==2:
+        for productoSeleccionado in productos: 
+            print("--------------------------------------------------")
+            print(f"El nombre del producto es: {productoSeleccionado['nombre']} \n El codigo del producto es: {productoSeleccionado['codigo']}\n La descripcion del producto es: {productoSeleccionado['descripcion']} \n La foto del producto es: {productoSeleccionado['foto']} \n El precio del producto es: {productoSeleccionado['precio']} \n La cantidad del producto es: {productoSeleccionado['cantidadEnBodega']} \n La fecha cuando entro a bodega es: {productoSeleccionado['fechaEntradaBodega']}")
+            print("--------------------------------------------------")            
+    elif opcion ==3:
+        pass
+    elif opcion ==4:
+        pass
+    elif opcion ==5:
+        pass
+    elif opcion ==6:
+        pass
+    else:
+        print("Digite una opcion valida, vuelva a intentar la")
